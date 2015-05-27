c02b31d226fc814d42a346e1bec2ce345ecfe617
croacker <7RV5699400>
2015-05-27 11:13:36 +0300

    Work with

--- src/main/java/ru/peak/ml/apdk/service/apdk/Cancel.java
+++ src/main/java/ru/peak/ml/apdk/service/apdk/Cancel.java
@@ -11,7 +11,8 @@ import ru.peak.ml.loyalty.message.ResponseMessage;
  */
 public class Cancel extends CommonApdkMessage {
 
-  public static byte[] ORIGINAL_DATA = {2, 81, 0, 0, 1, 0, 7, 1, 8, 0, 49, 48, 48, 48, 48, 48, 49, 56, 2, 4, 0, 49, 50, 51, 52, 5, 14, 0, 50, 48, 49, 53, 48, 53, 50, 48, 49, 52, 48, 56, 48, 51, 6, 8, 0, 51, 48, 49, 48, 49, 48, 49, 48, 17, 6, 0, 49, 49, 54, 48, 48, 49, 18, 3, 0, 50, 48, 81, 19, 3, 0, 0, 0, 38, 25, 1, 0, 52, 100, 3, 0, 48, 48, 48, 38, -80};
+    public static byte[] ORIGINAL_DATA = {2, 81, 0, 0, 1, 0, 7, 1, 8, 0, 49, 48, 48, 48, 48, 48, 49, 56, 2, 4, 0, 49, 50, 51, 52, 5, 14, 0, 50, 48, 49, 53, 48, 53, 50, 48, 49, 52, 48, 56, 48, 51, 6, 8, 0, 51, 48, 49, 48, 49, 48, 49, 48, 17, 6, 0, 49, 49, 54, 48, 48, 49, 18, 3, 0, 50, 48, 81, 19, 3, 0, 0, 0, 38, 25, 1, 0, 52, 100, 3, 0, 48, 48, 48, 38, -80};
+    public static byte[] ORIGINAL_DATA_1 = {2, 81, 0, 0, 1, 0, 7, 1, 8, 0, 49, 48, 48, 48, 48, 48, 49, 56, 2, 4, 0, 49, 50, 51, 52, 5, 14, 0, 50, 48, 49, 53, 48, 52, 50, 49, 49, 55, 49, 49, 50, 50, 6, 8, 0, 51, 48, 49, 48, 49, 48, 49, 48, 17, 6, 0, 49, 49, 54, 48, 48, 49, 18, 3, 0, 50, 48, 81, 19, 3, 0, 0, 0, 63, 25, 1, 0, 52, 100, 3, 0, 48, 48, 48, 36, 71};
 
     public static final String OPERATION_TYPE = new String(new byte[]{52});
 
@@ -19,11 +20,12 @@ public class Cancel extends CommonApdkMessage {
     @Setter
     private String date;
 
-  @Getter
-  @Setter
-  private String operationNumber;
+    @Getter
+    @Setter
+    private String operationNumber;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private String loyaltySum;
 
     public Cancel(String serverAddress, int serverPort) {
@@ -41,7 +43,7 @@ public class Cancel extends CommonApdkMessage {
     public ResponseMessage getNewApdkMessage() {
         ResponseMessage message = super.getNewApdkMessage();
         message.setOperationTimestamp(getDate());
-      message.setOperationNumber(getOperationNumber());
+        message.setOperationNumber(getOperationNumber());
         message.setSumLoyalty(Long.valueOf(getLoyaltySum()));
         message.setRewardSum(getLoyaltySum());
 
--- src/main/java/ru/peak/ml/apdk/service/apdk/ReturnSale.java
+++ src/main/java/ru/peak/ml/apdk/service/apdk/ReturnSale.java
@@ -11,6 +11,8 @@ import ru.peak.ml.loyalty.message.ResponseMessage;
  */
 public class ReturnSale extends CommonApdkMessage {
 
+    public static byte[] ORIGINAL_DATA = {2, 114, 0, 0, 1, 0, 7, 1, 8, 0, 49, 48, 48, 48, 48, 48, 49, 56, 2, 4, 0, 49, 50, 51, 52, 4, 3, 0, 54, 52, 51, 5, 14, 0, 50, 48, 49, 53, 48, 53, 50, 54, 49, 54, 51, 48, 48, 52, 6, 8, 0, 51, 48, 49, 48, 49, 48, 49, 48, 7, 12, 0, 49, 48, 48, 48, 48, 48, 48, 49, 48, 48, 48, 51, 16, 1, 0, 48, 17, 6, 0, 49, 49, 54, 48, 48, 49, 18, 3, 0, 50, 48, 81, 19, 3, 0, 0, 0, 71, 23, 4, 0, 49, 50, 48, 48, 25, 2, 0, 50, 57, 100, 3, 0, 48, 48, 48, -105, -24};
+
     public static final String OPERATION_TYPE = new String(new byte[]{50, 57});
 
     MessageFormatter formatter = new ReturnFormatter();
